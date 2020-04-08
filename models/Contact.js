74e4dbcb63f750c8721f5904d775e6fe091ce5f1
rizwanishaq <rizwanishaq@gmail.com>
2020-04-08 12:36:11 +0200

    backend completed

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: "personal",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("contact", ContactSchema);
