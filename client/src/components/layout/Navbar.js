62b3cefd0f6166f1e4ff9484e15933e82997d553
rizwanishaq <rizwanishaq@gmail.com>
2020-04-14 17:34:25 +0200

    Prepare for deploy

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
+
+function Navbar({ title = "Contact Keeper", icon = "fas fa-id-card-alt" }) {
+  const { isAuthenticated, logout, user } = useContext(AuthContext);
+  const { clearContacts } = useContext(ContactContext);
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+}
+
+export default Navbar;
